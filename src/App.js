b43c9684a75239ce179b339a3b8518118d92c413
deraj21 <deraj21@gmail.com>
2018-05-17 23:30:37 -0700

    done through step 4

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
 import React, { Component } from "react";
 import "./App.css";
+import { connect } from 'react-redux';
+import { increment, decrement } from './ducks/counter';
 
 class App extends Component {
   render() {
+    let { currentValue, increment, decrement } = this.props;
+
     return (
       <div className="app">
         <section className="counter">
-          <h1 className="counter__current-value">{ 0 }</h1>
+          <h1 className="counter__current-value">{ currentValue }</h1>
           <div className="counter__button-wrapper">
             <button
               className="counter__button increment-one"
-              onClick={ () => null }
+              onClick={ () => increment(1) }
             >
               +1
             </button>
             <button
               className="counter__button increment-five"
-              onClick={ () => null }
+              onClick={ () => increment(5) }
             >
               +5
             </button>
             <button
               className="counter__button decrement-one"
-              onClick={ () => null }
+              onClick={ () => decrement(1) }
             >
               -1
             </button>
             <button
               className="counter__button decrement-five"
-              onClick={ () => null }
+              onClick={ () => decrement(5) }
             >
               -5
             </button>
@@ -59,4 +63,12 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps( state ) {
+  return state;
+}
+
+export default connect( mapStateToProps, { increment, decrement } )( App );
+
+
+/*
+ */
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import store from './store';
 
 import "./index.css";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById( 'root' ));
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>
+  , document.getElementById( 'root' ));
+
+
+/*
+Open src/index.js.\
+Add a store prop that equals our imported store.
+ */
